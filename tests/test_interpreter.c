a7030adcea941d7a2ef0af26779b1a0f1a40a9a3
JeanPascal134 <jeanpascallompo03@gmail.com>
2026-02-01 15:15:04 +0100

    Update test_interpreter.c

---
+++ tests/test_interpreter.c
@@ -0,0 +1,25 @@
+void test_interpret_program() {
+    // Création de quelques instructions
+    Statement statements[3];
+
+    // SET x = 10
+    statements[0].type = SET;
+    statements[0].identifier = "x";
+    statements[0].value = 10;
+
+    // CALL algo1
+    statements[1].type = CALL;
+    statements[1].identifier = "algo1";
+
+    // RETURN 0
+    statements[2].type = RETURN;
+    statements[2].value = 0;
+
+    // Création du programme
+    Program program;
+    program.statements = statements;
+    program.statement_count = 3;
+
+    // Interprétation
+    interpret_program(&program);
+}
